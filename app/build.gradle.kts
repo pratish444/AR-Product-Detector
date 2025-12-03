39d1f790b7d60263694249a72848f8be25c4b57e
pratish444 <pratishk444@gmail.com>
2025-12-03 15:29:25 +0530

    Added Android resource files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.arproductdetector"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.arproductdetector"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
